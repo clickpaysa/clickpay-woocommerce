87f330bf46203cd09c0d415823ea0d54543a6dd8
Riyaztechsupport <135695828+Riyaztechsupport@users.noreply.github.com>
2023-12-28 12:15:42 +0300

    Add files via upload

---
+++ clickpay-woocommerce.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * @package ClickPay_PayPage
+ */
+
+/**
+ * Plugin Name:   ClickPay - WooCommerce Payment Gateway
+ * Plugin URI:    https://ClickPay.com.sa/
+ * Description:   ClickPay is a <strong>3rd party payment gateway</strong>. Ideal payment solutions for your internet business.
+
+ * Version:       4.21.0
+ * Requires PHP:  7.0
+ * Author:        ClickPay
+ * Author URI:    support@clickPay.com.sa
+ */
+
+if (!function_exists('add_action')) {
+  exit;
+}
+
+
+define('CLICKPAY_PAYPAGE_VERSION', '4.21.0');
+define('CLICKPAY_PAYPAGE_DIR', plugin_dir_path(__FILE__));
+define('CLICKPAY_PAYPAGE_ICONS_URL', plugins_url("icons/", __FILE__));
+define('CLICKPAY_PAYPAGE_IMAGES_URL', plugins_url("images/", __FILE__));
+define('CLICKPAY_DEBUG_FILE', WP_CONTENT_DIR . "/debug_clickpay.log");
+define('CLICKPAY_HTACCESS_FILE', WP_CONTENT_DIR . "/.htaccess");
+define('CLICKPAY_DEBUG_FILE_URL', get_bloginfo('url') . "/wp-content/debug_clickpay.log");
+
+define('CLICKPAY_PAYPAGE_METHODS', [
+  'mada'       => 'WC_Gateway_ClickPay_Mada',
+  'all'        => 'WC_Gateway_ClickPay_All',
+  'creditcard' => 'WC_Gateway_ClickPay_Creditcard',
+  'applepay'   => 'WC_Gateway_ClickPay_Applepay',
+  'amex'       => 'WC_Gateway_ClickPay_Amex',
+  'tabby'       => 'WC_Gateway_ClickPay_Tabby',
+]);
+
+require_once CLICKPAY_PAYPAGE_DIR . 'includes/clickpay_core.php';
+require_once CLICKPAY_PAYPAGE_DIR . 'includes/clickpay_functions.php';
+
+// Plugin activated
+register_activation_hook(__FILE__, 'woocommerce_clickpay_activated');
+
+// Load plugin function when woocommerce loaded
+add_action('plugins_loaded', 'woocommerce_clickpay_init', 0);
+
+//
+
+function woocommerce_clickpay_init()
+{
+
+  if (!class_exists('WooCommerce') || !class_exists('WC_Payment_Gateway')) {
+    add_action('admin_notices', 'woocommerce_clickpay_missing_wc_notice');
+    return;
+  }
+
+  define('WooCommerce2', !woocommerce_clickpay_version_check('4.0'));
+
+  // PT
+  require_once CLICKPAY_PAYPAGE_DIR . 'includes/clickpay_payment_methods.php';
+  require_once CLICKPAY_PAYPAGE_DIR . 'includes/clickpay_gateways.php';
+  require_once CLICKPAY_PAYPAGE_DIR . 'includes/clickpay_payment_token.php';
+
+
+
+  /**
+   * Add the Gateway to WooCommerce
+   **/
+  function woocommerce_add_clickpay_gateway($gateways)
+  {
+    $clickpay_gateways = array_values(CLICKPAY_PAYPAGE_METHODS);
+    $gateways = array_merge($gateways, $clickpay_gateways);
+
+    return $gateways;
+  }
+
+  function clickpay_filter_gateways($load_gateways)
+  {
+    if (is_admin()) return $load_gateways;
+
+    $gateways = [];
+    $currency = get_woocommerce_currency();
+
+    foreach ($load_gateways as $gateway) {
+
+      $code = array_search($gateway, CLICKPAY_PAYPAGE_METHODS);
+
+      if ($code) {
+        $allowed = ClickpayHelper::paymentAllowed($code, $currency);
+        if ($allowed) {
+          $gateways[] = $gateway;
+        }
+      } else {
+        // Not Clickpay Gateway
+        $gateways[] = $gateway;
+      }
+    }
+
+    return $gateways;
+  }
+
+
+  /**
+   * Add URL link to Clickpay plugin name pointing to WooCommerce payment tab
+   */
+  function clickpay_add_action_links($links)
+  {
+    $settings_url = admin_url('admin.php?page=wc-settings&tab=checkout');
+
+    $links[] = "<a href='{$settings_url}'>Settings</a>";
+
+    return $links;
+  }
+
+  add_filter('woocommerce_payment_gateways', 'woocommerce_add_clickpay_gateway');
+  add_filter('woocommerce_payment_gateways', 'clickpay_filter_gateways', 10, 1);
+  add_filter('woocommerce_payment_methods_list_item', 'get_account_saved_payment_methods_list_item_clickpay', 10, 2);
+  add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'clickpay_add_action_links');
+
+}
+
+
+function woocommerce_clickpay_activated()
+{
+  ClickpayHelper::log("Activate hook.", 1);
+  woocommerce_clickpay_check_log_permission();
+}
